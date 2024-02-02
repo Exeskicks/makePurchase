ffa6385a9a00408554d345b87e9563a93b7eebbe
Exeskicks <danil.yudin.2014@list.ru>
2024-02-02 22:26:28 +0700

    Загружаем фотографии в базу данных

--- src/main/java/com/example/makePurchase/models/Product.java
+++ src/main/java/com/example/makePurchase/models/Product.java
@@ -5,6 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "products")
 @Data
@@ -12,7 +27,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @Column(name = "title")
@@ -25,4 +40,20 @@ public class Product {
     private String city;
     @Column(name = "author")
     private String author;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER,
+            mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long preview_image_id;
+    //private LocalDateTime dateOfCreated;
+
+    //@PrePersist
+    //private void init() {
+    //   dateOfCreated = LocalDateTime.now();
+    //}
+
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
 }
